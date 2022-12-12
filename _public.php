430bf09921926e291876c633d0768fb55780f750
Jean-Christian Denis <contact@jcdenis.fr>
2022-12-12 21:18:33 +0100

    update to dotclear 2.24

--- _public.php
+++ _public.php
@@ -1,4 +1,5 @@
 <?php
+
 # ***** BEGIN LICENSE BLOCK *****
 #
 # AltoWithCSS3
@@ -6,34 +7,32 @@
 # License: http://creativecommons.org/licenses/by-nc/2.0/fr/
 #
 # ***** END LICENSE BLOCK *****
-if (!defined('DC_RC_PATH')) { return; }
-
-l10n::set(dirname(__FILE__).'/locales/'.$_lang.'/main');
-
-# appel css menu
-$core->addBehavior('publicHeadContent','altowithcss3menu_publicHeadContent');
-
-function altowithcss3menu_publicHeadContent($core)
-{
-	$style = $core->blog->settings->themes->altowithcss3_menu;
-	if (!preg_match('/^menu|simplemenu|menuno$/',$style)) {
-		$style = 'simplemenu';
-	}
-
-	$url = $core->blog->settings->system->themes_url.'/'.$core->blog->settings->system->theme;
-	echo '<link rel="stylesheet" type="text/css" media="screen" href="'.$url."/css/".$style.".css\" />\n";
+if (!defined('DC_RC_PATH')) {
+    return;
 }
 
-# appel css largeurs (1024.css ou 880.css)
-$core->addBehavior('publicHeadContent','altowithcss3width_publicHeadContent');
+l10n::set(dirname(__FILE__) . '/locales/' . dcCore::app()->lang. '/main');
 
-function altowithcss3width_publicHeadContent($core)
+# appel css menu
+dcCore::app()->addBehavior('publicHeadContent', 'altowithcss3_publicHeadContent');
+
+function altowithcss3_publicHeadContent()
 {
-	$style = $core->blog->settings->themes->altowithcss3_width;
-	if (!preg_match('/^880|1024$/',$style)) {
-		$style = '1024';
-	}
+    // menu
+    $style = dcCore::app()->blog->settings->themes->altowithcss3_menu;
+    if (!preg_match('/^menu|simplemenu|menuno$/', (string) $style)) {
+        $style = 'simplemenu';
+    }
 
-	$url = $core->blog->settings->system->themes_url.'/'.$core->blog->settings->system->theme;
-	echo '<link rel="stylesheet" type="text/css" media="screen" href="'.$url."/css/".$style.".css\" />\n";
+    $url = dcCore::app()->blog->settings->system->themes_url . '/' . dcCore::app()->blog->settings->system->theme;
+    echo '<link rel="stylesheet" type="text/css" media="screen" href="' . $url . '/css/' . $style . ".css\" />\n";
+
+    // width
+    $style = dcCore::app()->blog->settings->themes->altowithcss3_width;
+    if (!preg_match('/^880|1024$/', (string) $style)) {
+        $style = '1024';
+    }
+
+    $url = dcCore::app()->blog->settings->system->themes_url . '/' . dcCore::app()->blog->settings->system->theme;
+    echo '<link rel="stylesheet" type="text/css" media="screen" href="' . $url . '/css/' . $style . ".css\" />\n";
 }
